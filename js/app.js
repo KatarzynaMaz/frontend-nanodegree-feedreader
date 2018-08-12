a9b47847173788dd742006672545f4a45f8300d0
Katarzyna Mazur <mazur66@hotmail.com>
2018-08-12 01:57:26 -0400

    Modified one url that wasn't working

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var allFeeds = [
         url: 'http://blog.udacity.com/feed'
     }, {
         name: 'CSS Tricks',
-        url: 'http://feedburner.com/CssTricks'
+        url: 'http://feeds.feedburner.com/CssTricks'
     }, {
         name: 'HTML5 Rocks',
         url: 'http://feeds.feedburner.com/html5rocks'
